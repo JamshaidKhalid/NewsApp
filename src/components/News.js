713375e9693ecc25a29a8894a5194948a2ca08b9
Jamshaid Khalid <jamshaidkhalid32@gmail.com>
2022-04-08 01:48:33 +0500

    news card done

--- src/components/News.js
+++ src/components/News.js
@@ -4,8 +4,23 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        <NewsItem/>
+      <div className='container my-3'>
+        <h2>Top Headlines</h2>
+
+        <div className='row'>
+          <div className='col-md-4'>
+            <NewsItem title="News Title" description="News Description..." />
+          </div>
+
+          <div className='col-md-4'>
+            <NewsItem title="News Title" description="News Description..." />
+          </div>
+
+          <div className='col-md-4'>
+            <NewsItem title="News Title" description="News Description..." />
+          </div>
+        </div>
+
       </div>
     )
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,8 +2,18 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+    let {title, description} = this.props;
     return (
-      <div>NewsItem</div>
+      <div>
+        <div className="card" style={{ width: "18rem" }}>
+          <img src="https://sportshub.cbsistatic.com/i/r/2022/04/06/f0431276-ea50-48a4-9ac7-a374cb7ca499/thumbnail/1200x675/cc6ce66b63998821c0057ff9a642594d/masters-leaderboard-2022-preview-g.jpg" className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href="/" className="btn btn-primary">Continue Reading</a>
+          </div>
+        </div>
+      </div>
     )
   }
 }
