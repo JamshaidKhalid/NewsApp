23ccfcca6a06a05cd583b651047267bfdb270568
Jamshaid Khalid <jamshaidkhalid32@gmail.com>
2022-04-08 23:33:58 +0500

    news api done

--- src/components/News.js
+++ src/components/News.js
@@ -2,25 +2,37 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+
+
+  constructor() {
+    super();
+    this.state = {
+      articles: []
+    }
+  }
+
+  async componentDidMount() {
+    let url = "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=236b2c5df73a4507990536033f2ceb33";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+
+    this.setState({ articles: parsedData.articles });
+  }
+
+
   render() {
     return (
       <div className='container my-3'>
         <h2>Top Headlines</h2>
 
         <div className='row'>
-          <div className='col-md-4'>
-            <NewsItem title="News Title" description="News Description..." imageUrl="https://sportshub.cbsistatic.com/i/r/2022/04/06/f0431276-ea50-48a4-9ac7-a374cb7ca499/thumbnail/1200x675/cc6ce66b63998821c0057ff9a642594d/masters-leaderboard-2022-preview-g.jpg" />
-          </div>
-
-          <div className='col-md-4'>
-            <NewsItem title="News Title" description="News Description..." />
-          </div>
-
-          <div className='col-md-4'>
-            <NewsItem title="News Title" description="News Description..." />
-          </div>
+          {this.state.articles.map((element) => {
+            
+            return <div className='col-md-4'>
+              <NewsItem title={element.title?element.title.slice(0, 35):""} description={element.description?element.description.slice(0, 58):""} imageUrl={element.urlToImage?element.urlToImage:"https://www.team-bhp.com/sites/default/files/styles/large/public/03%20L%20to%20R%20-%20Refreshed%20Sonet%20Seltos.jpg"} />
+            </div>
+          })}
         </div>
-
       </div>
     )
   }
